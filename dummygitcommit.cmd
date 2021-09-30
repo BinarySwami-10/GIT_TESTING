05c01da95b1e5bc3210eabd384606f7ed455d560
Nikhil-Swami <54004431+BinarySwami-10@users.noreply.github.com>
2021-09-30 07:20:13 +0530

    This was committed by cloud commit AWS, helps to greenify github activity log

--- dummygitcommit.cmd
+++ dummygitcommit.cmd
@@ -1,5 +1,5 @@
 echo off
 echo %date% @ %time% > tempfile.tmp
 git add -A
-git commit -m "Cron Commit"
+git commit -m "This was committed by cloud commit AWS, helps to greenify github activity log"
 git push origin master
