2705f0afd559b04208a18dafb96dbedc404b7880
Nikhil-Swami <54004431+BinarySwami-10@users.noreply.github.com>
2021-11-30 02:12:45 +0530

    This was committed by cloud commit AWS, helps to greenify github activity log

---
+++ gitcron.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd /root/
+if [ ! -d "$DIRECTORY" ]; then
+  git clone https://github.com/BinarySwami-10/GitCron.git
+fi
+
+cd GitCron
+git config credential.helper 'store'
+date > tempfile.tmp
+git add -A
+git commit -m "SOURCE: AWS cloud Cron commit | INSTANCE_ID:$(cat /etc/hostname) | INTENT:helps to greenify github activity log"
+git push origin master   
